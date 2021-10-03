9660c4ac3c04664671b08d57611e6c139dda58b8
Ivan Kobtsev <vanomc77@gmail.com>
2021-10-03 14:13:19 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace csharp_group_homework_64_ivan_kobtsev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
